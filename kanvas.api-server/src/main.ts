8127a3077df0a763725cae331d763b9a8897fc9d
Rick Klomp <rick.klomp@tzconnect.com>
2021-11-09 15:19:34 +0100

    2 global scripts added: setup.bash (install dependencies) and run.bash

--- kanvas.api-server/src/main.ts
+++ kanvas.api-server/src/main.ts
@@ -5,6 +5,6 @@ import { config } from 'dotenv'
 async function bootstrap() {
   config()
   const app = await NestFactory.create(AppModule, { cors: true })
-  await app.listen(3000)
+  await app.listen(process.env['KANVAS_API_PORT'] || 3000)
 }
 bootstrap()
