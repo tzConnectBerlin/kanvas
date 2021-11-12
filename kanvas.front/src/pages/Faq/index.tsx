4218bd18202710a2cd1bf06a1a602c4b3dafbf49
Ricardo Côrtes <giralander@hotmail.com>
2021-11-12 11:23:47 +0100

    Added test with more subitem

--- kanvas.front/src/pages/Faq/index.tsx
+++ kanvas.front/src/pages/Faq/index.tsx
@@ -24,7 +24,7 @@ const StyledAnchorLink = styled.a <{ theme?: Theme }>`
     }
 
 `
- 
+
 const StyledStack = styled(Stack) <{ theme?: Theme }>`
     max-width: 100rem;
     width: 100%;
@@ -71,6 +71,12 @@ const StyledStack = styled(Stack) <{ theme?: Theme }>`
             padding-left: 1rem;
         }
     }
+
+    .faq-content {
+        .MuiTypography-body2 {
+            margin-bottom: 1rem;
+        }
+    }
 `
 const StyledSection = styled.section`
 margin-bottom: 4rem;
@@ -103,148 +109,82 @@ const StickyNav = styled.nav`
 const Faq: FC<FaqProps> = () => {
     const { t } = useTranslation(['translation']);
 
-    // const faqItems = [
-    //     {
-    //         headline: t('faq.01_headline'),
-    //         text: t('faq.01_text'),
-    //         className: '',
-    //         id: '#one'
-    //     },
-    //     {
-    //         headline: t('faq.01_1_headline'),
-    //         text: t('faq.01_1_text'),
-    //         className: 'sub',
-    //         id: '#one_one'
-    //     },
-    //     {
-    //         headline: t('faq.01_2_headline'),
-    //         text: t('faq.01_2_text'),
-    //         className: 'sub',
-    //         id: '#one_two'
-    //     },
-    //     {
-    //         headline: t('faq.02_headline'),
-    //         text: t('faq.02_text'),
-    //         className: '',
-    //         id: '#two'
-    //     },
-    //     {
-    //         headline: t('faq.03_headline'),
-    //         text: t('faq.03_text'),
-    //         className: '',
-    //         id: '#three'
-    //     }
-    // ];
+    const faqItems = [
+        {
+            headline: t('faq.01_headline'),
+            text: t('faq.01_text'),
+            className: '',
+            id: '#one'
+        },
+        {
+            headline: t('faq.01_1_headline'),
+            text: t('faq.01_1_text'),
+            className: 'sub',
+            id: '#one_one'
+        },
+        {
+            headline: t('faq.01_2_headline'),
+            text: t('faq.01_2_text'),
+            className: 'sub',
+            id: '#one_two'
+        },
+        {
+            headline: t('faq.02_headline'),
+            text: t('faq.02_text'),
+            className: '',
+            id: '#two'
+        },
+        {
+            headline: t('faq.03_headline'),
+            text: t('faq.03_text'),
+            className: '',
+            id: '#three'
+        },
+        {
+            headline: t('faq.03_1_headline'),
+            text: t('faq.03_1_text'),
+            className: 'sub',
+            id: '#three_one'
+        }
+    ];
 
     return (
         <PageWrapper>
             <StyledStack direction='column' spacing={3}>
                 <FlexSpacer minHeight={10} />
 
-                <Grid container spacing={2} >
+                <Grid container spacing={2} className="faq-nav">
                     <Grid item xs={12} md={5} sx={{ position: 'relative' }}>
                         <StickyNav>
                             <Scrollspy
-                                ids={["one", "one_one", "one_two", "two", "three"]}
+                                ids={faqItems.map(item => item.id)}
                                 itemContainerClassName="scrollNavContainer"
                                 activeItemClassName="isCurrent"
                                 itemClassName="navItemClass"
                                 includeParentClasses={true}
-                                itemElement={<li className="MuiTypography-root MuiTypography-body" />}
+                                itemElement={<li className="MuiTypography-root MuiTypography-body2" />}
                             />
-
-                            {/* {faqItems.map((nav) => <>
-                                    <li key={nav.id} >
-                                        <a href={nav.id}>
-                                            {nav.headline}
-                                        </a>
-                                    </li>
-                                </>
-                                )
-                                } */}
-
                         </StickyNav>
                     </Grid>
 
-                    <Grid item xs={12} md={7}>
+                    <Grid item xs={12} md={7} className="faq-content">
                         <Typography size="h1" weight='SemiBold'>{t('faq.headline')}</Typography>
                         <FlexSpacer minHeight={3} />
 
-                        {/* {faqItems.map((node) => <>
-                            <StyledSection >
-                                <Typography id={node.id}size="h2" weight='SemiBold' className={node.className}>
+                        {faqItems.map((node) => <>
+                            <StyledSection>
+                                <Typography id={node.id} size="h2" weight='SemiBold' className={node.className}>
                                     {node.headline}
                                 </Typography>
-                                <Typography size="body" weight='Light' >
-                                    {node.text}
-                                    <br />
-                                    What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). Where does it come from? Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham. Where can I get some? There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
-                                </Typography>    
+                                <Typography size="body2" weight='Light' >
+                                    {node.text}                                                                       
+                                </Typography>
+                                <Typography size="body2" weight='Light' >
+                                    {node.text}                                                                       
+                                </Typography>
                             </StyledSection>
-                        </>
-                        )
-                        }                        
-                        /* */}
-
-                        <StyledSection>
-                            <Typography id="one" size="h2" weight='SemiBold' >
-                                {t('faq.01_headline')}
-                            </Typography>
-                            <Typography size="body2" weight='Light'>
-                                {t('faq.01_text')}
-                            </Typography>
-                            <Typography size="body2" weight='Light'>
-                                <p>{t('common.lorenIpsumLong')}</p>
-                            </Typography>
-                        </StyledSection>
-
-                        <StyledSection>
-                            <Typography id="one_one" size="h2" className="sub" weight='SemiBold'>
-                                {t('faq.01_1_headline')}
-                            </Typography>
-                            <Typography size="body2" weight='Light'>
-                                {t('faq.01_1_text')}
-                            </Typography>
-                            <Typography size="body2" weight='Light'>
-                                <p>{t('common.lorenIpsumLong')}</p>
-                            </Typography>
-                        </StyledSection>
-
-                        <StyledSection>
-                            <Typography id="one_two" size="h2" className="sub" weight='SemiBold'>
-                                {t('faq.01_2_headline')}
-                            </Typography>
-                            <Typography size="body2" weight='Light'>
-                                {t('faq.01_2_text')}
-                            </Typography>
-                            <Typography size="body2" weight='Light'>
-                                <p>{t('common.lorenIpsumLong')}</p>
-                            </Typography>
-                        </StyledSection>
-
-                        <StyledSection>
-                            <Typography id="two" size="h2" weight='SemiBold' >
-                                {t('faq.02_headline')}
-                            </Typography>
-                            <Typography size="body2" weight='Light'>
-                                {t('faq.02_text')}
-                            </Typography> 
-                            <Typography size="body2" weight='Light'>
-                                <p>{t('common.lorenIpsumLong')}</p>
-                            </Typography>
-                        </StyledSection>
-
-                        <StyledSection>
-                            <Typography id="three" size="h2" weight='SemiBold'>
-                                {t('faq.03_headline')}
-                            </Typography>
-                            <Typography size="body" weight='Light'>
-                                {t('faq.03_text')}
-                            </Typography>  
-                            <Typography size="body" weight='Light'>
-                                {t('common.lorenIpsumLong')}
-                            </Typography>
-                        </StyledSection>
+                        </>)
+                        }                    
                     </Grid>
                 </Grid>
             </StyledStack>
