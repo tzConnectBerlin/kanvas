b28312d05615ab57b883921f2fc5fc86e612db54
Rick Klomp <rick.klomp@tzconnect.com>
2021-11-17 18:23:22 +0100

    small fixes in DOWN step of new migration

--- kanvas.api-server/migrations/1637166133-session_cart.sql
+++ kanvas.api-server/migrations/1637166133-session_cart.sql
@@ -26,6 +26,7 @@ create table mtm_cart_session_nft(
   primary key (cart_session_id, nft_id)
 );
 
+-- bugfix. editions number should definitely be set
 ALTER TABLE nft ALTER COLUMN editions_size SET DEFAULT 1;
 UPDATE nft SET editions_size = COALESCE(editions_size, 1);
 ALTER TABLE nft ALTER COLUMN editions_size SET NOT NULL;
@@ -39,8 +40,10 @@ BEGIN;
 ALTER TABLE nft_category DROP CONSTRAINT fk_parent;
 ALTER TABLE nft_category ADD CONSTRAINT fk_nft FOREIGN KEY (parent) REFERENCES nft(id);
 
-DROP TABLE cart_session;
+ALTER TABLE kanvas_user DROP COLUMN cart_session;
+
 DROP TABLE mtm_cart_session_nft;
+DROP TABLE cart_session;
 
 ALTER TABLE nft ALTER COLUMN editions_size DROP NOT NULL;
 ALTER TABLE nft ALTER COLUMN editions_size DROP DEFAULT;
