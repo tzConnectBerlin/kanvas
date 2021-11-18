71f505ee939965d4ae09d0a7416b2d6d61a0a3c6
Rick Klomp <rick.klomp@tzconnect.com>
2021-11-18 17:59:33 +0100

    move cookie-session into a single middleware, and logger middleware

---
+++ kanvas.api-server/src/middleware/cookie_session.ts
@@ -0,0 +1,21 @@
+import { Injectable, NestMiddleware } from '@nestjs/common'
+import cookieSession = require('cookie-session')
+import { v4 as uuidv4 } from 'uuid'
+import { assertEnv } from '../utils'
+
+@Injectable()
+export class CookieSessionMiddleware implements NestMiddleware {
+  private readonly options = {
+    secret: assertEnv('JWT_SECRET'),
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  }
+
+  use(req: any, resp: any, next: any): void {
+    cookieSession(this.options)(req, resp, () => {
+      if (typeof req.session.uuid === 'undefined') {
+        req.session.uuid = uuidv4()
+        next()
+      }
+    })
+  }
+}
