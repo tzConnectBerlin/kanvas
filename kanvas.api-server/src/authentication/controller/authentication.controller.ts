8772e8ae1602aba9bc5f6bfe848628e30942d389
Rick Klomp <rick.klomp@tzconnect.com>
2021-11-18 17:00:17 +0100

    bugfix: don't expose /users/create (should only be called through /auth)

--- kanvas.api-server/src/authentication/controller/authentication.controller.ts
+++ kanvas.api-server/src/authentication/controller/authentication.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common'
+import {
+  HttpStatus,
+  HttpException,
+  Logger,
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+} from '@nestjs/common'
 import { CurrentUser } from 'src/decoraters/user.decorator'
 import { UserEntity } from 'src/user/entity/user.entity'
 import { JwtAuthGuard } from '../guards/jwt-auth.guard'
 import { AuthenticationService } from '../service/authentication.service'
+import { PG_UNIQUE_VIOLATION_ERRCODE } from '../../constants'
 
 @Controller('auth')
 export class AuthenticationController {
@@ -21,6 +31,21 @@ export class AuthenticationController {
 
   @Post('register')
   async register(@Body() user: UserEntity): Promise<any> {
-    return this.authService.register(user)
+    return this.authService.register(user).catch((err: any) => {
+      if (err?.code === PG_UNIQUE_VIOLATION_ERRCODE) {
+        throw new HttpException(
+          'User with these credentials already exists',
+          HttpStatus.BAD_REQUEST,
+        )
+      }
+
+      Logger.error(
+        `Error on creating user=${JSON.stringify(user)}, err: ${err}`,
+      )
+      throw new HttpException(
+        'Something went wrong',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      )
+    })
   }
 }
