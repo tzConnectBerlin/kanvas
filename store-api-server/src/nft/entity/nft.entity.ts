221661d0424bd0824aceb2206fc7276561ca3523
Rick Klomp <rick.klomp@tzconnect.com>
2021-11-24 11:53:03 +0100

    add comment to the code about https://github.com/tzConnectBerlin/kanvas/pull/82/files#r755922552

--- store-api-server/src/nft/entity/nft.entity.ts
+++ store-api-server/src/nft/entity/nft.entity.ts
@@ -10,7 +10,7 @@ export interface NftEntity {
   contract: string
   tokenId: string
   categories: CategoryEntity[]
-  editionsAvailable?: number
+  editionsAvailable?: number // note: for now it's optional because it only applies to the store, not to the admin part
   status: string // corresponds to NftStateTransitionConfig
 }
 
