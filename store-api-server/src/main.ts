db9b3c829935753dfffb9d3995bdb1e4f749faca
Rick Klomp <rick.klomp@tzconnect.com>
2021-11-24 19:15:53 +0100

    Remove the cors related stuff from the backend (handled above by nginx)

--- store-api-server/src/main.ts
+++ store-api-server/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true })
+  const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   await app.listen(process.env['KANVAS_API_PORT'] || 3000)
 }
