ed237c405ced37a71bb48176347e9c5f523e0fc5
Rick Klomp <rick.klomp@tzconnect.com>
2021-11-26 17:47:40 +0100

    remove import *

--- store-api-server/src/s3.service.ts
+++ store-api-server/src/s3.service.ts
@@ -1,12 +1,12 @@
 import { Logger, Injectable } from '@nestjs/common'
 import { Result, Err, Ok } from 'ts-results'
-import * as AWS from 'aws-sdk'
+import { S3 } from 'aws-sdk'
 
 const AWS_S3_BUCKET = process.env.AWS_S3_BUCKET
 
 @Injectable()
 export class S3Service {
-  private s3 = new AWS.S3({
+  private s3 = new S3({
     accessKeyId: process.env.AWS_S3_ACCESS_KEY,
     secretAccessKey: process.env.AWS_S3_KEY_SECRET,
   })
