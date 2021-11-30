45b9cf6b251932cfe6c09d745c61574373bf4c74
Rick Klomp <rick.klomp@tzconnect.com>
2021-11-30 18:40:10 +0100

    additionally return 'createdAt' in NftEntity

--- store-api-server/src/nft/entity/nft.entity.ts
+++ store-api-server/src/nft/entity/nft.entity.ts
@@ -3,6 +3,7 @@ import { CategoryEntity } from 'src/category/entity/category.entity'
 // TODO let's replace this and all other models with just a simple immutable key/value object - preferably using Immutable.js
 export interface NftEntity {
   id: number
+  createdAt: number
   name: string
   ipfsHash: string
   metadata: any // TODO expand on the meaning of this type
