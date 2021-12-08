d5e4b4b78bb208ce043a1f7a9c459b7a03851744
Rick Klomp <rick.klomp@tzconnect.com>
2021-12-08 19:50:27 +0100

    better explicit scoping of dynamic javascript + extensions.ts

--- lib/src/expr.ts
+++ lib/src/expr.ts
@@ -1,12 +1,27 @@
 import { Result, Ok, Err } from 'ts-results';
 import * as log from 'log';
 import { Nft } from './types';
+import * as ext from './extensions';
 
 export function evalExpr<T>(nftState: Nft, s: string, defaultOnErr: T): T {
+  //const scopedEval = (scope: any, script: string) =>
+  // Function(`"use strict"; ${script}`).bind(scope)();
+
   log.debug(`evaluation '${s}'`);
   try {
     const nft = nftState.attributes;
-    const res = eval(s);
+
+    //const res = eval(s);
+    //const res = scopedEval({ nft: nftState.attributes, ext: ext }, s);
+    const res = Function(
+      `
+"use strict";
+function test(nft, ext) {
+  ${s};
+}
+return test(this.nft, this.ext)`,
+    ).bind({ nft: nft, ext: ext })();
+
     if (typeof defaultOnErr === 'undefined') {
       // T is void, just accept any return value/type here, useful for execExpr.
       return res;
---
+++ lib/src/extensions.ts
@@ -0,0 +1,3 @@
+export function testFunction() {
+  console.log('called!');
+}
