a0be62328da04249b887fb54d7ca259bbc391ea7
shekhar-shubhendu <shubh.shekhar1991@gmail.com>
2021-12-13 12:06:02 +0100

    feat: add nft controller

--- admin-api-server/src/nft/nft.controller.ts
+++ admin-api-server/src/nft/nft.controller.ts
@@ -6,35 +6,52 @@ import {
   Patch,
   Param,
   Delete,
+  Request,
+  Query,
+  UseGuards,
 } from '@nestjs/common';
-import { NftService } from './nft.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { ParseJSONArrayPipe } from 'src/pipes/ParseJSONArrayPipe';
+import { FilterParams } from 'src/types';
 import { NftDto } from './dto/nft.dto';
+import { NftService } from './nft.service';
 
 @Controller('nft')
 export class NftController {
   constructor(private readonly nftService: NftService) {}
 
   @Post()
-  create(@Body() createNftDto: NftDto) {
-    return this.nftService.create(createNftDto);
+  @UseGuards(JwtAuthGuard)
+  create(@Request() req, @Body() createNftDto: NftDto) {
+    return this.nftService.create(req.user, createNftDto);
   }
 
   @Get()
-  findAll() {
-    return this.nftService.findAll();
+  @UseGuards(JwtAuthGuard)
+  findAll(
+    @Query('sort', new ParseJSONArrayPipe())
+    sort?: string[],
+    @Query('filter', new ParseJSONArrayPipe()) filter?: FilterParams,
+    @Query('range', new ParseJSONArrayPipe())
+    range?: number[],
+  ) {
+    return this.nftService.findAll({ sort, filter, range });
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
     return this.nftService.findOne(+id);
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateNftDto: NftDto) {
     return this.nftService.update(+id, updateNftDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.nftService.remove(+id);
   }
