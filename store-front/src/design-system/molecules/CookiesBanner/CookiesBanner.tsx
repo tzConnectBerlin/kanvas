cf6d6b5f597266cce07be6edeabe46c779c691c0
Tristan Nicolaides <66777395+TrNicolaides@users.noreply.github.com>
2021-12-14 18:54:37 +0100

    modification for the cookie banners

--- store-front/src/design-system/molecules/CookiesBanner/CookiesBanner.tsx
+++ store-front/src/design-system/molecules/CookiesBanner/CookiesBanner.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
-import { Grid, Paper, useMediaQuery, useTheme } from '@mui/material';
+import { Grid, Paper, Stack, useMediaQuery, useTheme } from '@mui/material';
 import { CustomButton } from '../../atoms/Button';
 import { Typography } from '../../atoms/Typography';
 import { useTranslation } from 'react-i18next';
@@ -10,6 +10,7 @@ import FlexSpacer from '../../atoms/FlexSpacer';
 
 interface StyledPaperProps {
     backgroundColor?: string;
+    theme?: Theme;
 }
 
 export interface CookiesBannerProps {
@@ -32,16 +33,16 @@ export interface CookiesBannerProps {
     handleClose?: (title: string) => void | Promise<void>;
 }
 
-const StyledLink = styled(Link)<{ theme?: Theme }>`
+const StyledLink = styled(Link) <{ theme?: Theme }>`
     color: inherit;
     text-decoration: none;
     font-family: inherit;
     display: inline;
     text-decoration: underline;
 `;
-const StyledPaper = styled(Paper)<StyledPaperProps>`
-    background-color: #ccc;
-    border-radius: 0.25rem;
+const StyledPaper = styled(Paper) <StyledPaperProps>`
+    background-color: ${propos => propos.theme.palette.background.paper};
+    border-radius: 0 !important;
     color: #fff;
     position: sticky;
     left: 0;
@@ -51,35 +52,18 @@ const StyledPaper = styled(Paper)<StyledPaperProps>`
     box-shadow: 0 -4px 6px -2px rgba(9, 9, 9, 0.2);
 `;
 
-const StyledGrid = styled(Grid)<{ isMobile: boolean }>`
-    justify-content: ${({ isMobile }) => (isMobile ? 'center' : 'flex-start')};
-    align-items: center;
+const StyledStack = styled(Stack)`
 
-    > * {
-        width: fit-content;
-        &:not(:last-child) {
-            margin-right: ${({ isMobile }) => (isMobile ? 'initial' : '2rem')};
-        }
-
-        &:not(:first-child) {
-            margin-bottom: ${({ isMobile }) =>
-                isMobile ? '1.5rem' : 'initial'};
-        }
-    }
-`;
+    justify-content: center !important;
+    align-items: center !important;
+    padding: 2.5rem;
+`
 
 const defaultCookies = {
     necessary: true,
     preferences: false,
     statistics: false,
     marketing: false,
-};
-
-const fullCookies = {
-    necessary: true,
-    preferences: true,
-    statistics: true,
-    marketing: true,
 };
 
 export const CookiesBanner: React.FC<CookiesBannerProps> = ({
@@ -91,7 +75,7 @@ export const CookiesBanner: React.FC<CookiesBannerProps> = ({
     const { t } = useTranslation(['translation']);
 
     const { primary } = theme.palette;
-    const isMobile = useMediaQuery('(max-width:600px)');
+    const isMobile = useMediaQuery('(max-width:874px)');
     const isDesktop = useMediaQuery('(min-width:1150px)');
     const [cookie, setCookie] = React.useState(standard);
     const addCookie = (cookies: { [name: string]: boolean }) => {
@@ -105,46 +89,30 @@ export const CookiesBanner: React.FC<CookiesBannerProps> = ({
 
     return (
         <StyledPaper elevation={0} backgroundColor={primary.main}>
-            <Grid container direction="row" width="100%">
-                <Grid
-                    item
-                    xs={12}
-                    container
-                    direction={isMobile ? 'column' : 'row'}
-                    padding={isMobile ? '1rem' : '3rem 3rem 2rem'}
-                    flexGrow={1}
+            <StyledStack direction={{ xs:"column", md:"row" }} spacing={2}>
+                <Typography
+                    width={isMobile ? '90%' : '45%' }
+                    size='body2'
+                    weight="Light"
+                    display="inline"
+                    style={{
+                        alignSelf: 'center',
+                        marginBottom: !isDesktop ? '2rem' : 0,
+                        marginRight: isDesktop ? 'auto' : '2rem',
+                    }}
                 >
-                    <Typography
-                        size={isMobile ? 'body' : 'h4'}
-                        weight="Light"
-                        display="inline"
-                        style={{
-                            alignSelf: 'center',
-                            marginBottom: !isDesktop ? '2rem' : 0,
-                            marginRight: isDesktop ? 'auto' : '2rem',
-                        }}
-                    >
-                        {t('cookies.text')}
-                        <StyledLink to="/privacy">
-                            {t('cookies.link')}
-                        </StyledLink>
-                        {' .'}
-                    </Typography>
-
-                    <CustomButton
-                        fullWidth={!!isMobile}
-                        variant="text"
-                        color="secondary"
-                        type="submit"
-                        onClick={handleSubmit}
-                        label={t('cookies.button')}
-                        style={{
-                            order: isMobile ? 99 : 0,
-                            color: theme.palette.primary.main,
-                        }}
-                    ></CustomButton>
-                </Grid>
-            </Grid>
+                    {t('cookies.text')}
+                    <StyledLink to="/privacy">
+                        {t('cookies.link')}
+                    </StyledLink>
+                    {' .'}
+                </Typography>
+                <CustomButton
+                    size='small'
+                    onClick={handleSubmit}
+                    label={t('cookies.button')}
+                />
+            </StyledStack>
         </StyledPaper>
     );
 };
