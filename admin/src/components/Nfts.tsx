724d8a6640d796d6760bb2c9ca46a96a40f721fa
shekhar-shubhendu <shubh.shekhar1991@gmail.com>
2021-12-15 12:45:25 +0100

    feat: nft endpoint integration

--- admin/src/components/Nfts.tsx
+++ admin/src/components/Nfts.tsx
@@ -8,73 +8,48 @@ import {
   Create,
   ImageInput,
   ImageField,
-  SelectArrayInput,
-  ChipField,
-  SingleFieldList,
+  DateField,
 } from 'react-admin';
-import { TextArrayField } from './TextArrayField';
 
 export const NftList = ({ ...props }) => (
   <List {...props}>
     <Datagrid rowClick="edit">
       <TextField source="id" />
-      <TextField source="name" />
-      <TextField source="description" />
-      <ImageField source="image" />
-      <TextArrayField source="categories">
-        <SingleFieldList>
-          <ChipField />
-        </SingleFieldList>
-      </TextArrayField>
-      <TextField source="status" />
+      <TextField source="nftName" label="Name" />
+      <TextField source="nftState" label="Current State" />
+      <ImageField source="dataUri" label="Image" />
+      <DateField source="createdAt" label="Created at" showTime />
+      <DateField source="updatedAt" label="Last updated at" showTime />
+      <TextField source="disabled" label="Disabled" />
     </Datagrid>
   </List>
 );
 
 export const NftEdit = ({ ...props }) => {
-  const categories = [
-    { id: 'blue', name: 'Blue' },
-    { id: 'apple', name: 'Apple' },
-    { id: 'cat', name: 'Cat' },
-    { id: 'scooter', name: 'scooter' },
-  ];
-
   return (
     <Edit {...props}>
       <SimpleForm>
         <TextInput source="id" disabled />
-        <TextInput source="name" />
+        <TextInput source="nftName" />
         <TextInput source="description" />
         <ImageInput source="image" accept="image/*">
           <ImageField source="src" title="title" />
         </ImageInput>
-        <SelectArrayInput source="categories" choices={categories} />
-        {props.permissions.includes(2) && <TextInput source="status" />}
       </SimpleForm>
     </Edit>
   );
 };
 
 export const NftCreate = ({ ...props }) => {
-  const categories = [
-    { id: 'blue', name: 'Blue' },
-    { id: 'apple', name: 'Apple' },
-    { id: 'cat', name: 'Cat' },
-    { id: 'scooter', name: 'scooter' },
-  ];
-  return props.permissions.includes(3) ? (
+  return (
     <Create {...props}>
       <SimpleForm>
-        <TextInput source="name" />
-        <TextInput source="description" />
+        <TextInput source="id" disabled />
+        <TextInput source="nftName" />
         <ImageInput source="image" accept="image/*">
           <ImageField source="src" title="title" />
         </ImageInput>
-        <SelectArrayInput source="categories" choices={categories} />
-        <TextInput source="status" defaultValue="initial" disabled />
       </SimpleForm>
     </Create>
-  ) : (
-    <>'Only Creators can create NFTs'</>
   );
 };
