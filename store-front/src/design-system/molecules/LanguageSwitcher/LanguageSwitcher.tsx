b5c5de0e439fe5486398afdace1c25ea364a257a
Ricardo Côrtes <giralander@hotmail.com>
2021-12-15 14:42:38 +0100

    Added fix for color on field focus

--- store-front/src/design-system/molecules/LanguageSwitcher/LanguageSwitcher.tsx
+++ store-front/src/design-system/molecules/LanguageSwitcher/LanguageSwitcher.tsx
@@ -4,44 +4,82 @@ import {
     Button,
     FormControl,
     InputLabel,
-    MenuItem,
     Select,
+    Theme,
+    useTheme,
 } from '@mui/material';
 import { SelectChangeEvent } from '@mui/material/Select';
-import { Box } from '@mui/system';
+import { Box, styled } from '@mui/system';
 import { useTranslation } from 'react-i18next';
 
-export const LanguageSwitcher = () => {
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core/styles';
+
+interface LanguageSwitcherProps {
+    theme?: Theme;
+}
+
+const useStyles = makeStyles({
+    root: {
+        width: 200,
+        '& .MuiOutlinedInput-input': {
+            color: 'currentColor',
+        },
+        '& .MuiInputLabel-root': {
+            color: 'currentColor',
+        },
+        '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+            borderColor: 'currentColor',
+        },
+        '&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+            borderColor: 'currentColor',
+        },
+        '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input': {
+            color: 'purple',
+        },
+        '& .MuiInputLabel-root.Mui-focused': {
+            color: 'purple',
+        },
+        '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':
+            {
+                borderColor: 'purple',
+            },
+    },
+});
+
+const StyledDiv = styled(FormControl)<{ theme?: Theme }>`
+    max-width: 8rem;
+`;
+
+export const LanguageSwitcher: FC<LanguageSwitcherProps> = ({ ...props }) => {
     const { t } = useTranslation(['translation']);
-
     const [language, setLanguage] = useState('');
+    const classes = useStyles();
 
-    const handleChange = (event: SelectChangeEvent) => {
-        setLanguage(event.target.value as string);
+    const handleTextfield = (event: any) => {
+        setLanguage(event.target.value);
         i18next.changeLanguage(event.target.value as string);
     };
 
     return (
-        <Box sx={{ maxWidth: 140 }}>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">
-                    {t('common.language')}
-                </InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={language}
-                    label={t('common.language')}
-                    onChange={handleChange}
-                    sx={{
-                        borderRadius: 0,
-                    }}
-                >
-                    <MenuItem value="en">English</MenuItem>
-                    <MenuItem value="fr">French</MenuItem>
-                    <MenuItem value="ab">Arabic</MenuItem>
-                </Select>
-            </FormControl>
-        </Box>
+        <StyledDiv>
+            <TextField
+                className={classes.root}
+                id="simple-select"
+                value={language}
+                label={t('common.language')}
+                variant="outlined"
+                onChange={handleTextfield}
+                select
+            >
+                <MenuItem value="">
+                    <em>None</em>
+                </MenuItem>
+                <MenuItem value="en">English</MenuItem>
+                <MenuItem value="fr">French</MenuItem>
+                <MenuItem value="ab">Arabic</MenuItem>
+            </TextField>
+        </StyledDiv>
     );
 };
