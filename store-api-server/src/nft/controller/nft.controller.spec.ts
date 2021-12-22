5450d078817f7cb7a63710c30505b8561ef7be94
Rick Klomp <rick.klomp@tzconnect.com>
2021-12-22 14:50:36 +0100

    wip

--- store-api-server/src/nft/controller/nft.controller.spec.ts
+++ store-api-server/src/nft/controller/nft.controller.spec.ts
@@ -1,8 +1,10 @@
 require('dotenv').config();
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { NftController } from './nft.controller';
 import { DbMockModule } from '../../db_mock.module';
 import { NftService } from '../service/nft.service';
+import { PaginationParams } from '../params';
 
 describe('NftController', () => {
   let controller: NftController;
@@ -20,4 +22,45 @@ describe('NftController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  const expHttpStatusTests = [
+    {
+      name: '',
+      params: <PaginationParams>{ page: 0 },
+      expStatusCode: 401,
+    },
+  ];
+
+  for (const { name, params, expStatusCode } of expHttpStatusTests) {
+    it(`should return ${expStatusCode} for .get(${params})`, async () => {
+      expectErrWithHttpStatus(expStatusCode, async () => {
+        await controller.getFiltered(params).rejects;
+        //try {
+        //  await controller.getFiltered(params);
+        //} catch (err: any) {
+        //  throw err;
+        //}
+      });
+    });
+  }
 });
+
+async function expectErrWithHttpStatus(
+  expStatusCode: number,
+  f: () => Promise<any>,
+): Promise<void> {
+  try {
+    await f();
+  } catch (err: any) {
+    if (!(err instanceof HttpException)) {
+      throw `expected HttpException, got: ${err}`;
+    }
+    const gotStatusCode = err.getStatus();
+    expect(gotStatusCode).toEqual(expStatusCode);
+    if (gotStatusCode !== expStatusCode) {
+      throw `expected HttpException with status=${expStatusCode}, got ${gotStatusCode} with err: ${err}`;
+    }
+    return;
+  }
+  throw `expected HttpException, got no error`;
+}
