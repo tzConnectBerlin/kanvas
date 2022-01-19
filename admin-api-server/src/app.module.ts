caff1506805623f21820aa7cb4dadfe47d5f249b
RKlompUU <rick_klomp@hotmail.com>
2022-01-19 11:42:23 +0100

    add rate limiter to admin-api and make it env set-able

--- admin-api-server/src/app.module.ts
+++ admin-api-server/src/app.module.ts
@@ -1,4 +1,6 @@
+import { APP_GUARD } from '@nestjs/core';
 import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -6,11 +8,24 @@ import { UserModule } from './user/user.module';
 import { NftModule } from './nft/nft.module';
 import { LoggerMiddleware } from './middleware/logger';
 import { CookieSessionMiddleware } from './middleware/cookie_session';
+import { ProxiedThrottlerGuard } from './decoraters/proxied_throttler';
+import { RATE_LIMIT_WINDOW_SECS, RATE_LIMIT } from 'src/constants';
 
 @Module({
-  imports: [AuthModule, UserModule, NftModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    NftModule,
+    ThrottlerModule.forRoot({
+      ttl: RATE_LIMIT_WINDOW_SECS,
+      limit: RATE_LIMIT,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_GUARD, useClass: ProxiedThrottlerGuard },
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer): void {
--- admin-api-server/src/constants.ts
+++ admin-api-server/src/constants.ts
@@ -3,3 +3,8 @@ export const PG_UNIQUE_VIOLATION_ERRCODE = '23505';
 export const AUTH_SALT_ROUNDS = 10;
 export const NFT_IMAGE_MAX_BYTES: number = 1000 * 1000 * 2;
 export const NFT_IMAGE_PREFIX = 'NFT_IMAGE_';
+
+export const RATE_LIMIT_WINDOW_SECS = Number(
+  process.env['RATE_LIMIT_WINDOW_SECS'] || 60,
+);
+export const RATE_LIMIT = Number(process.env['RATE_LIMIT'] || 100);
