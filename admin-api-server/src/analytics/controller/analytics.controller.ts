0e5cb1ecb904a9144f2fbc8fd55859d6198cab51
RKlompUU <rick_klomp@hotmail.com>
2022-01-27 16:31:07 +0100

    add auth guards around the analytics endpoints (and only access for admin)

--- admin-api-server/src/analytics/controller/analytics.controller.ts
+++ admin-api-server/src/analytics/controller/analytics.controller.ts
@@ -1,11 +1,10 @@
 import {
-  Body,
   Controller,
   Get,
-  Post,
   Query,
   HttpException,
   HttpStatus,
+  UseGuards,
 } from '@nestjs/common';
 import {
   MetricEntity,
@@ -14,11 +13,17 @@ import {
 } from '../../analytics/entity/analytics.entity';
 import { AnalyticsService } from '../service/analytics.service';
 import { enumFromStringValue } from 'src/utils';
+import { Roles } from 'src/role/role.decorator';
+import { Role } from 'src/role/role';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RolesGuard } from 'src/role/role.guard';
 
 @Controller('analytics')
 export class AnalyticsController {
   constructor(private analyticsService: AnalyticsService) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Get('sales/priceVolume/snapshot')
   async salesPriceVolume(
     @Query('resolution') resolutionStr?: string,
@@ -27,6 +32,8 @@ export class AnalyticsController {
     return await this.analyticsService.getSnapshotSalesPriceVolume(params);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Get('sales/nftCount/snapshot')
   async salesNftCount(
     @Query('resolution') resolutionStr?: string,
@@ -35,6 +42,8 @@ export class AnalyticsController {
     return await this.analyticsService.getSnapshotSalesNftCount(params);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Get('sales/priceVolume/timeseries')
   async timeseriesSalesPriceVolume(
     @Query('resolution') resolutionStr?: string,
@@ -45,6 +54,8 @@ export class AnalyticsController {
     };
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Get('sales/nftCount/timeseries')
   async timeseriesSalesNftCount(
     @Query('resolution') resolutionStr?: string,
