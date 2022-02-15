f07bce22f589ff45221ea8a4d2f2c5ccaad39328
Ricardo Côrtes <giralander@hotmail.com>
2022-02-15 12:38:39 +0100

    Added serviceWorker to src folder

--- store-front/src/index.tsx
+++ store-front/src/index.tsx
@@ -2,8 +2,9 @@ import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 import { IntlProvider } from 'react-intl';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 const locale = 'en';
 
@@ -16,9 +17,14 @@ const setupAndRender = async () => {
     );
 };
 
-setupAndRender();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+setupAndRender();
