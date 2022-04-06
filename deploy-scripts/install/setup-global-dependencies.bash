623338f60ba73e8be45e6f7b1cbe0e7d2ee862da
Rick Klomp <rick.klomp@tzconnect.com>
2022-04-06 12:24:06 +0200

    deployment scripts amend

--- deploy-scripts/install/setup-global-dependencies.bash
+++ deploy-scripts/install/setup-global-dependencies.bash
@@ -27,3 +27,28 @@ sudo apt-key add ACCC4CF8.asc
 
 # fetch the metadata from the new repo
 sudo apt-get update
+
+
+# need docker
+
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo gpasswd -a $USER docker
+
+echo "reboot or logout->login for sudoless docker"
