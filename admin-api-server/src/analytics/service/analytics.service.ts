9c692204db4ae99baa1cca965e080f1b8a2042d1
Rick Klomp <rick.klomp@tzconnect.com>
2022-04-07 18:22:55 +0200

    admin-api: bugfix, onchain activities timestamp at UTC
    caused by que pasa writing timestamps _with_ timezone into pg

--- admin-api-server/src/analytics/service/analytics.service.ts
+++ admin-api-server/src/analytics/service/analytics.service.ts
@@ -85,7 +85,7 @@ SELECT
   COUNT(1) OVER () AS total_activity_count
 FROM (
   SELECT
-    lvl.baked_at AS timestamp,
+    lvl.baked_at AT TIME ZONE 'UTC' AS timestamp,
     'mint' AS kind,
     NULL AS "from",
     owner AS "to",
@@ -101,7 +101,7 @@ FROM (
   UNION ALL
 
   SELECT
-    lvl.baked_at AS timestamp,
+    lvl.baked_at AT TIME ZONE 'UTC' AS timestamp,
     'transfer' AS kind,
     tr_from.from_ AS "from",
     tr_dest.to_ AS "to",
