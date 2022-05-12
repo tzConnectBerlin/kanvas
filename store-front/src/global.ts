98316a0fab457a6159a60266ebe25759057a0ced
Diego Peralta <diego.peralta@tzconnect.com>
2022-05-12 17:36:18 +0200

    feat(tezpay): upgrade node

--- store-front/src/global.ts
+++ store-front/src/global.ts
@@ -2,6 +2,6 @@ import { NetworkType } from '@airgap/beacon-sdk';
 import { Networks } from 'kukai-embed';
 
 export const RPC_URL =
-    process.env.REACT_APP_RPC_URL ?? 'https://ithaca-archive.tzconnect.berlin/';
+    process.env.REACT_APP_RPC_URL ?? 'https://ithacanet.ecadinfra.com/';
 export const NETWORK: keyof typeof NetworkType = 'ITHACANET';
 export const KUKAI_NETWORK: string = 'ithacanet';
