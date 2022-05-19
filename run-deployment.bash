e65eddcef337ed0a5296f852b4ca45478f76e807
Rick Klomp <rick.klomp@tzconnect.com>
2022-05-19 12:00:31 +0200

    make all docker services enabled by default

--- run-deployment.bash
+++ run-deployment.bash
@@ -5,5 +5,6 @@ export ADMIN_API_ENABLED=yes
 export STORE_API_ENABLED=yes
 export STORE_FRONT_ENABLED=yes
 export ADMIN_FRONT_ENABLED=yes
+export DEPLOYMENT=yes
 
-docker-compose up | tee backend.log
+docker-compose up | tee kanvas.log
