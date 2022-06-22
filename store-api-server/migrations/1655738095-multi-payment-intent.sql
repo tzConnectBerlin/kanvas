d17f968038fa817a6a860087b0f609bcf0cc8d6f
Rick Klomp <rick.klomp@tzconnect.com>
2022-06-22 12:45:27 +0200

    add down migration for multi payment intents

--- store-api-server/migrations/1655738095-multi-payment-intent.sql
+++ store-api-server/migrations/1655738095-multi-payment-intent.sql
@@ -41,7 +41,7 @@ ALTER TABLE payment DROP COLUMN expires_at;
 ALTER TABLE payment DROP CONSTRAINT payment_nft_order_id_uniq;
 CREATE INDEX payment_nft_order_id_idx ON payment(nft_order_id);
 
-ALTER TYPE payment_provider ADD VALUE 'wert';
+ALTER TYPE payment_provider ADD VALUE IF NOT EXISTS 'wert';
 
 
 COMMIT;
@@ -49,5 +49,36 @@ COMMIT;
 -- ==== DOWN ====
 
 BEGIN;
+
+ALTER TABLE nft_order ADD COLUMN currency TEXT;
+
+UPDATE nft_order SET currency = 'unknown';
+
+UPDATE nft_order
+SET currency = payment.currency
+FROM payment
+WHERE payment.nft_order_id = nft_order.id;
+
+ALTER TABLE nft_order ALTER COLUMN currency SET NOT NULL;
+
+ALTER TABLE payment DROP COLUMN amount;
+
+ALTER TABLE mtm_nft_order_nft ADD COLUMN price NUMERIC;
+ALTER TABLE __mtm_nft_order_nft_delisted ADD COLUMN price NUMERIC;
+
+ALTER TABLE payment DROP COLUMN currency;
+ALTER TABLE payment ADD COLUMN expires_at TIMESTAMP WITHOUT TIME ZONE;
+
+UPDATE payment
+SET expires_at = nft_order.expires_at
+FROM nft_order
+WHERE payment.nft_order_id = nft_order.id;
+
+ALTER TABLE payment ALTER COLUMN expires_at SET NOT NULL;
+
+ALTER TABLE nft_order DROP COLUMN expires_at;
+
+DROP INDEX payment_nft_order_id_idx;
+ALTER TABLE payment ADD CONSTRAINT payment_nft_order_id_uniq UNIQUE (nft_order_id);
 
 COMMIT;
