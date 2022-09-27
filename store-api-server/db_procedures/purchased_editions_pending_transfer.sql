05ed63df2b4557ecce0f1077bf4b5d5a588a56ed
Rick Klomp <rick.klomp@tzconnect.com>
2022-09-27 14:24:03 +0200

    create db procedures outside of migrations/, improves readability
    this prevents the need for copy pasting entire functions into a new
    migration file whenever the function needs to be updated. that approach
    had issues because it: A). decreased readability of code, as it wasnt
    necessarily obvious that one has to find the latest function variant,
    B). decreased visibility of how exactly a function is being changed in
    the related PR, because there is no direct code diff view.
    
    now all db procedures are defined in 1 consistent code location:
    store-api-server/db_procedures/

---
+++ store-api-server/db_procedures/purchased_editions_pending_transfer.sql
@@ -0,0 +1,31 @@
+DROP FUNCTION IF EXISTS purchased_editions_pending_transfer;
+
+CREATE FUNCTION purchased_editions_pending_transfer(
+  purchased_nft_id INTEGER, buyer_address TEXT, minter_address TEXT)
+RETURNS INTEGER
+AS $$
+  WITH onchain_registered AS (
+    SELECT
+      mint.command->'args'->>'amount' AS amount
+    FROM peppermint.operations AS mint
+    WHERE mint.command->>'handler' = 'nft'
+      AND mint.command->>'name'::TEXT = 'transfer'
+      AND (mint.command->'args'->'token_id')::INTEGER = purchased_nft_id
+      AND mint.command->'args'->>'from_address' = minter_address
+      AND mint.command->'args'->>'to_address' = buyer_address
+      AND EXISTS (
+        SELECT 1
+        FROM que_pasa.txs AS onchain_tx
+        WHERE onchain_tx.operation_hash = mint.included_in
+      )
+  )
+  SELECT
+    COUNT(mtm) -
+      COALESCE((SELECT SUM(amount::INTEGER) FROM onchain_registered), 0)
+      AS num_editions
+  FROM kanvas_user AS usr
+  JOIN mtm_kanvas_user_nft AS mtm
+    ON  mtm.kanvas_user_id = usr.id
+    AND mtm.nft_id = purchased_nft_id
+  WHERE usr.address = buyer_address
+$$ LANGUAGE SQL;
