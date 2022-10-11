9b5c1ea673dfc7a9f45eac92e063e9b3cd341b4b
Rick Klomp <rick.klomp@tzconnect.com>
2022-10-11 14:54:05 +0200

    store-api: add formats field to /nfts/create

--- store-api-server/test/isolated.e2e.ts
+++ store-api-server/test/isolated.e2e.ts
@@ -260,5 +260,120 @@ VALUES ($1, $2)
         },
       });
     });
+
+    it('NFT create with formats field specified', async () => {
+      const nftId = nftIds[nftIds.length - 1] + 1;
+      await testUtils.createNft(app, {
+        id: nftId,
+
+        name: 'test',
+        description: 'test description',
+
+        artifactUri: 'some_s3_uri',
+
+        price: 0.5,
+        categories: [10],
+        editionsSize: 4,
+
+        formats: {
+          artifact: {
+            mimeType: 'video/mp4',
+            width: 300,
+            height: 550,
+            duration: '00:10:15',
+          },
+          thumbnail: {
+            height: 450,
+          },
+        },
+      });
+
+      const resp = await request(app.getHttpServer()).get(`/nfts/${nftId}`);
+      expect(resp.statusCode).toEqual(200);
+      testUtils.logFullObject(resp.body);
+      expect(resp.body).toMatchObject({
+        id: nftId,
+
+        formats: {
+          artifact: {
+            mimeType: 'video/mp4',
+            width: 300,
+            height: 550,
+            duration: '00:10:15',
+          },
+          thumbnail: {
+            height: 450,
+          },
+        },
+      });
+    });
+
+    it('same formats fields specified results in normalized db representation', async () => {
+      const nftId = nftIds[nftIds.length - 1] + 1;
+      await testUtils.createNft(app, {
+        id: nftId,
+
+        name: 'test',
+        description: 'test description',
+
+        artifactUri: 'some_s3_uri',
+
+        price: 0.5,
+        categories: [10],
+        editionsSize: 4,
+
+        formats: {
+          artifact: {
+            mimeType: 'video/mp4',
+            width: 300,
+          },
+          thumbnail: {
+            height: 450,
+          },
+        },
+      });
+      await testUtils.createNft(app, {
+        id: nftId + 1,
+
+        name: 'test',
+        description: 'test description',
+
+        artifactUri: 'some_s3_uri',
+
+        price: 0.5,
+        categories: [10],
+        editionsSize: 4,
+
+        formats: {
+          artifact: {
+            mimeType: 'video/mp4', // same as above, not an additional format table row
+            width: 350, // differs, expecting a new format table row
+          },
+          thumbnail: {
+            height: 450, // same as above, not an additional format table row
+          },
+        },
+      });
+
+      await testUtils.withDbConn(async (db) => {
+        const res = (
+          await db.query(`
+SELECT
+  (SELECT count(1) FROM format) AS format_count,
+  (SELECT count(1) FROM mtm_nft_format) AS mtm_count`)
+        ).rows.map((row: any) => {
+          return {
+            formatCount: Number(row['format_count']),
+            mtmCount: Number(row['mtm_count']),
+          };
+        });
+        expect(res).toStrictEqual([
+          {
+            formatCount: 4,
+            mtmCount: 6,
+          },
+        ]);
+      });
+    });
   });
 }
