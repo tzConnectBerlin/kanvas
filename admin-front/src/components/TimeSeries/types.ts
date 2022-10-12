912762a12fbf29d3fcd1844aac483f8488a0af9d
joshuapruefer <joshua.pruefer@tzconnect.com>
2022-10-12 11:23:54 +0200

    * Introduce resolution and month range filters to timeseries data

---
+++ admin-front/src/components/TimeSeries/types.ts
@@ -0,0 +1,15 @@
+export type Resolution = 'hour' | 'day' | 'week' | 'month';
+export type Month =
+  | 'All'
+  | 'January'
+  | 'February'
+  | 'March'
+  | 'April'
+  | 'May'
+  | 'June'
+  | 'July'
+  | 'August'
+  | 'September'
+  | 'October'
+  | 'November'
+  | 'December';
