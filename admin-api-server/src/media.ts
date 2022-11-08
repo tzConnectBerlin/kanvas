a42cf9a49bfe32c6cb555b6f4f2219ade6f7d32f
Rick Klomp <rick.klomp@tzconnect.com>
2022-11-08 16:48:33 +0100

    adjust ipfs metadata gen to pick up on admin-api provided formats info

--- admin-api-server/src/media.ts
+++ admin-api-server/src/media.ts
@@ -5,14 +5,58 @@ import { isBottom, maybe } from './utils.js';
 import { Logger } from '@nestjs/common';
 import { imageSize } from 'image-size';
 
-export interface VideoMetadata {
+export interface ContentMetadata {
+  mimeType: string;
+  fileSize: number;
+
+  width?: number;
+  height?: number;
+
+  duration?: string;
+  dataRate?: { value: number; unit: string };
+}
+
+export async function getContentMetadata(
+  nftId: number,
+  file: any,
+): Promise<ContentMetadata> {
+  let res = {
+    mimeType: file.mimetype,
+    fileSize: file.size,
+  };
+  switch (file.mimetype.split('/')[0]) {
+    case 'video':
+      try {
+        const videoMetadata = await getVideoMetadata(file);
+        res = { ...res, ...videoMetadata };
+      } catch (err: any) {
+        Logger.warn(
+          `failed to inspect video metadata for nftId=${nftId}, err: ${err}`,
+        );
+      }
+      break;
+    case 'image':
+      try {
+        const imageMetadata = getImageMetadata(file);
+        res = { ...res, ...imageMetadata };
+      } catch (err: any) {
+        Logger.warn(
+          `failed to inspect image metadata for nftId=${nftId}, err: ${err}`,
+        );
+      }
+      break;
+  }
+  return res;
+}
+
+interface VideoMetadata {
   duration?: string;
   width?: number;
   height?: number;
   dataRate?: { value: number; unit: string };
 }
 
-export function getVideoMetadata(file: any): Promise<VideoMetadata> {
+function getVideoMetadata(file: any): Promise<VideoMetadata> {
   return new Promise((resolve) => {
     ffmpeg(intoStream(file.buffer)).ffprobe((err, metadata) => {
       resolve(videoMetadataFromFfprobe(err, metadata));
@@ -83,12 +127,12 @@ function durationString(totalSeconds: number): string {
   return `${padded(hours)}:${padded(minutes)}:${padded(seconds)}`;
 }
 
-export interface ImageMetadata {
+interface ImageMetadata {
   width?: number;
   height?: number;
 }
 
-export function getImageMetadata(file: any): ImageMetadata {
+function getImageMetadata(file: any): ImageMetadata {
   const dimensions = imageSize(file.buffer);
   return {
     width: dimensions.width,
