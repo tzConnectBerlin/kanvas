b2eb3439885bad9305493734cb2d8f84e545a43e
Justin <justin.calleja@williamhill.com>
2022-11-22 20:05:55 +0100

    Keep user.entity types in one file
    so FE can use it in build.

---
+++ store-api-server/src/user/entity/user.types.ts
@@ -0,0 +1,32 @@
+import type { NftEntity, NftEntityPage } from '../../nft/entity/nft.entity.js';
+
+export interface UserEntity {
+  id: number;
+  userAddress: string;
+  createdAt: number; // in UTC UNIX
+  profilePicture: string;
+  signedPayload?: string;
+  cartSession?: string;
+}
+
+export interface ProfileEntity {
+  user: UserEntity;
+  collection: NftEntityPage;
+  pendingOwnership: NftEntity[];
+}
+
+export interface UserCart {
+  expiresAt?: number;
+  nfts: NftEntity[];
+}
+
+export interface UserTotalPaid {
+  userId: number;
+  userPicture?: string;
+  totalPaid: string;
+}
+
+export interface NftOwnershipStatus {
+  nftId: number;
+  ownerStatuses: string[]; // List, because 1 user can have multiple editions of a single nft
+}
