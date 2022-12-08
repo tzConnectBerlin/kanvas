f7da369e82f510a0c61bae6c00661d34ff6de455
Rick Klomp <rick.klomp@tzconnect.com>
2022-12-08 14:20:09 +0100

    try setting secure and sameSite in API for the Set-Cookie headers

--- store-api-server/src/middleware/cookie_session.ts
+++ store-api-server/src/middleware/cookie_session.ts
@@ -2,13 +2,21 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import cookieSession from 'cookie-session';
 import { v4 as uuidv4 } from 'uuid';
 import { assertEnv } from '../utils.js';
+import { SECURE_COOKIE_SETTINGS } from '../constants.js';
 
 @Injectable()
 export class CookieSessionMiddleware implements NestMiddleware {
-  private readonly options = {
+  private options: any = {
     secret: assertEnv('JWT_SECRET'),
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
   };
+
+  constructor() {
+    if (SECURE_COOKIE_SETTINGS) {
+      this.options['sameSite'] = 'none';
+      this.options['secure'] = true;
+    }
+  }
 
   use(req: any, resp: any, next: any): void {
     cookieSession(this.options)(req, resp, () => {
