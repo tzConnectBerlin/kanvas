0d6756e25915710cad093b7b7a42a5bbf2c76c6d
Rick Klomp <rick.klomp@tzconnect.com>
2022-12-08 18:08:38 +0100

    apply same SECURE_COOKIE_SETTINGS env var to admin-api (it works)

--- admin-api-server/src/middleware/cookie_session.ts
+++ admin-api-server/src/middleware/cookie_session.ts
@@ -2,15 +2,23 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
 import { assertEnv } from '../utils.js';
 import { createRequire } from 'module';
+import { SECURE_COOKIE_SETTINGS } from '../constants.js';
 const require = createRequire(import.meta.url);
 const cookieSession = require('cookie-session');
 
 @Injectable()
 export class CookieSessionMiddleware implements NestMiddleware {
-  private readonly options = {
+  private options: any = {
     secret: assertEnv('JWT_SECRET'),
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
   };
+
+  constructor() {
+    if (SECURE_COOKIE_SETTINGS) {
+      this.options['sameSite'] = 'none';
+      this.options['secure'] = true;
+    }
+  }
 
   use(req: any, resp: any, next: any): void {
     cookieSession(this.options)(req, resp, () => {
