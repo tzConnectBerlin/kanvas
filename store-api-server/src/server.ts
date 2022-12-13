c3617d47b142b58af58942a62d43c6ffcc58f558
Lukas Schönbächler <l.schoenbaechler@papers.ch>
2022-12-14 00:05:27 +0100

    fix(security): hide json parsing errors

--- store-api-server/src/server.ts
+++ store-api-server/src/server.ts
@@ -6,6 +6,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module.js';
 import { BEHIND_PROXY, LOCAL_CORS } from './constants.js';
+import bodyParser from 'body-parser';
 
 export async function runKanvasServer() {
   const server = await NestFactory.create<NestExpressApplication>(
@@ -40,6 +41,15 @@ export function setupKanvasServer(server: NestExpressApplication) {
   if (BEHIND_PROXY) {
     server.set('trust proxy', 1);
   }
+
+  // Hide JSON parsing errors from user
+  server.use(bodyParser.json())
+  server.use((err:any, req:any, res:any, next:any) => {
+    if (err.status >= 400 && err.status < 500 &&
+      err.message.indexOf('JSON') !== -1) {
+        res.status(422).send()
+    }
+  })
 
   server.enableShutdownHooks();
 }
