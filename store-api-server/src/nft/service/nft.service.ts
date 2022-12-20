7284428313098d742ff37acefb6c1a83aba1fba0
Rick Klomp <rick.klomp@tzconnect.com>
2022-12-20 16:05:12 +0100

    bugfix create nft

--- store-api-server/src/nft/service/nft.service.ts
+++ store-api-server/src/nft/service/nft.service.ts
@@ -99,7 +99,7 @@ INSERT INTO nft (
 )
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, COALESCE(
     (SELECT created_at FROM nft WHERE id = $13), now() AT TIME ZONE 'UTC'
-  ))
+  ), $14, $15, $16)
       `,
 
         [
