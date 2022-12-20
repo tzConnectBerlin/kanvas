89c8fdd3326ffb030ac16ef2f32f22e63be33f77
Rick Klomp <rick.klomp@tzconnect.com>
2022-12-20 10:44:36 +0100

    nft_editions_locked optimization: dont need to check cart for unfolds

--- store-api-server/db_procedures/nft_editions_locked.sql
+++ store-api-server/db_procedures/nft_editions_locked.sql
@@ -1,22 +1,20 @@
-CREATE OR REPLACE FUNCTION nft_editions_locked(nft_id INT)
+CREATE OR REPLACE FUNCTION nft_editions_locked(nft_id_input INT)
   RETURNS TABLE(reserved BIGINT, owned BIGINT)
   STABLE PARALLEL SAFE
 AS $$
   WITH nft_ids AS (
     SELECT
-      nft_id AS id
+      nft_id_input AS id
     UNION ALL
     SELECT
       unfold_nft_id AS id
     FROM proxy_unfold
-    WHERE proxy_nft_id = nft_id
+    WHERE proxy_nft_id = nft_id_input
   )
   SELECT
     (
       SELECT count(1) AS num
-      FROM nft_ids
-      JOIN mtm_cart_session_nft AS cart_nfts
-        ON cart_nfts.nft_id = nft_ids.id
+      FROM mtm_cart_session_nft AS cart_nfts
       JOIN cart_session AS cart
         ON cart.id = cart_nfts.cart_session_id
       LEFT JOIN nft_order
@@ -24,16 +22,22 @@ AS $$
       LEFT JOIN payment
         ON  payment.nft_order_id = nft_order.id
         AND payment.status IN ('created', 'promised', 'processing')
-      WHERE payment.id IS NULL
+      WHERE cart_nfts.nft_id = nft_id_input
+        AND payment.id IS NULL
     ) + (
-      SELECT count(DISTINCT nft_order.id) AS num
-      FROM nft_order
-      JOIN mtm_nft_order_nft AS order_nfts
-        ON  order_nfts.nft_order_id = nft_order.id
-        AND order_nfts.nft_id = $1
-      JOIN payment
-        ON  payment.nft_order_id = nft_order.id
-        AND payment.status IN ('created', 'promised', 'processing')
+      SELECT count(DISTINCT order_nfts.id) AS num
+      FROM mtm_nft_order_nft AS order_nfts
+      WHERE order_nfts.nft_id = nft_id_input
+        AND EXISTS (
+        SELECT 1
+        FROM payment
+        WHERE payment.nft_order_id = order_nfts.nft_order_id
+          AND payment.status IN ('created', 'promised', 'processing', 'succeeded')
+      ) AND NOT EXISTS (
+        SELECT 1
+        FROM nft_order_delivery AS delivery
+        WHERE delivery.nft_order_id = order_nfts.nft_order_id
+      )
     ) AS reserved,
     (SELECT count(1) FROM nft_ids JOIN mtm_kanvas_user_nft ON nft_id = nft_ids.id) AS owned
 $$ LANGUAGE SQL;
